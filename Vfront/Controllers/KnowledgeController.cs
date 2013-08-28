4c2f6035a41aa08787259c80bc4a6bea9ac8b86a
Vivi Jiang <mobojiang@gmail.com>
2013-08-28 14:43:48 +0800

    set up env

---
+++ Vfront/Controllers/KnowledgeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vfront.Controllers
+{
+    public class KnowledgeController : Controller
+    {
+        //
+        // GET: /Knowledge/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Javascript()
+        {
+            return View();
+        }
+
+    }
+}
