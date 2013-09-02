b9e1807b0cab1676ebcf2ea542b9d59d7c1582a2
Vivi Jiang <mobojiang@gmail.com>
2013-09-02 18:11:01 +0800

    Add more pages

---
+++ Vfront/Controllers/EfController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vfront.Controllers
+{
+    public class EfController : Controller
+    {
+        //
+        // GET: /Team/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EfParents()
+        {
+            return View();
+        }
+        public ActionResult MediaHub()
+        {
+            return View();
+        }
+        public ActionResult Odin()
+        {
+            return View();
+        }
+        public ActionResult Poster()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vfront/Controllers/ElastislideController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vfront.Controllers
+{
+    public class ElastislideController : Controller
+    {
+        //
+        // GET: /Elastislide/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
